57227990cc4dc60be224266058098dec80689f56
Dave Sutherland <dave@daveography.ca>
2019-09-19 15:31:54 -0600

    Clean up package and tasks

---
+++ src/client/index.ts
@@ -0,0 +1,5 @@
+export * from "./soda-client";
+export * from "./soda-context";
+export * from "./soda-host";
+export * from "./soda-resource-id";
+export * from "./soda-resource";
---
+++ src/soql/clauses/index.ts
@@ -0,0 +1,6 @@
+export * from "./clause";
+export * from "./column";
+export * from "./limit-clause";
+export * from "./offset-clause";
+export * from "./select";
+export * from "./where";
