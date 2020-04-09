6bbb6a058a1b422966b63af19cb616e0302fb539
Dave Sutherland <dave@daveography.ca>
2020-04-09 16:27:21 -0600

    Add module definition

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SodaClient } from './client';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 export * from "./client";
 export * from "./datatypes";
 export * from "./soql-query";
+
+@NgModule({
+  providers: [SodaClient],
+  imports: [CommonModule, HttpClientModule],
+  exports: [HttpClientModule]
+})
+export class SodaClientModule {
+  static forRoot(): ModuleWithProviders<SodaClientModule> {
+    return {
+      ngModule: SodaClientModule
+    };
+  }
+}
