15c32aea3b0ec221aee3a5e3f33d09a69549c46b
Dave Sutherland <dave@daveography.ca>
2020-07-14 14:26:49 -0600

    Update within-box & circle to work with Point type

--- spec/soql-query-builder/clauses/where/functions/within-circle.spec.ts
+++ spec/soql-query-builder/clauses/where/functions/within-circle.spec.ts
@@ -1,16 +1,35 @@
+import { Point } from "geojson";
 import { Location } from "../../../../../src/datatypes/location";
 import { Column } from "../../../../../src/soql-query-builder/clauses/column";
 import { WithinCircle } from "../../../../../src/soql-query-builder/clauses/where/functions/within-circle";
 
 describe("Within Circle Where Filter", () => {
 
-  it("should create within_circle where filter", () => {
+  it("should create within_circle where filter with Location", () => {
     const filterObj = new WithinCircle(
       new Column("col1"),
       new Location(53.528525, -113.501720),
-      500,
-      );
+      500
+    );
     expect(filterObj.toString())
       .toEqual("within_circle(col1, 53.528525, -113.501720, 500)");
+  });
+
+  it("should create within_circle where filter with Point", () => {
+    const point: Point = {
+      type: "Point",
+      coordinates: [
+        -113.501720,
+        53.528525
+      ]
+    };
+
+    const filterObj = new WithinCircle(
+      new Column("col1"),
+      point,
+      500
+    );
+    expect(filterObj.toString())
+      .toEqual("within_circle(col1, 'POINT (-113.50172 53.528525)', 500)");
   });
 });
--- src/soql-query-builder/clauses/where/functions/within-box.ts
+++ src/soql-query-builder/clauses/where/functions/within-box.ts
@@ -1,19 +1,29 @@
+import { Point } from 'geojson';
 import { Location } from '../../../../datatypes/location';
+import { WellKnownType } from '../../../../datatypes/well-known-type';
+import { LocationUtils } from '../../../../utilities/location-utils';
 import { Column } from '../../column';
 import { IWhereComponent } from '../where-component';
 
-export class WithinBox implements IWhereComponent {
+export class WithinBox<T extends Location | Point> implements IWhereComponent {
   public readonly Column: Column;
-  public readonly Start: Location;
-  public readonly End: Location;
+  public readonly Start: T;
+  public readonly End: T;
 
-  constructor(column: Column, start: Location, end: Location) {
+  constructor(column: Column, start: T, end: T) {
     this.Column = column;
     this.Start = start;
     this.End = end;
   }
 
   public toString(): string {
-    return `within_box(${this.Column}, ${this.Start}, ${this.End})`;
+    if (LocationUtils.isLocation(this.Start)) {
+      return `within_box(${this.Column}, ${this.Start}, ${this.End})`;
+    }
+    else {
+      const wktStart = new WellKnownType(this.Start as Point);
+      const wktEnd = new WellKnownType(this.End as Point);
+      return `within_box(${this.Column}, '${wktStart}', '${wktEnd}')`;
+    }
   }
 }
