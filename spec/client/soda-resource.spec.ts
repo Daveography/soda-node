8d0e0e762a809a878f79591b72db4778febdd690
Dave Sutherland <dave@daveography.ca>
2020-07-16 16:52:14 -0600

    Add and simplify SodaResource tests

--- spec/client/soda-resource.spec.ts
+++ spec/client/soda-resource.spec.ts
@@ -1,22 +1,22 @@
-import { SodaHost } from "../../src/client/soda-host-decorator";
-import { SodaContext } from '../../src/client/soda-context';
-import { SodaDataset } from "../../src/client/soda-dataset-decorator";
 import { SodaResource } from "../../src/client";
+import { SodaDataset } from "../../src/client/soda-dataset-decorator";
 
 describe("SodaResource", () => {
 
   @SodaDataset("abcd-efgh")
-  class SampleResource { }
+  class SampleDataset { }
 
-  // tslint:disable-next-line: max-classes-per-file
-  @SodaHost("https://data.opendata.org/")
-  class SampleContext extends SodaContext {
-    constructor() { super(null); }
-    res = new SodaResource(SampleResource, this);
-  }
+  class BadDataset { }
 
-  it("should add set resource id from decorated resource class", () => {
-    const context = new SampleContext();
-    expect(context.res.Id.toString()).toEqual("abcd-efgh");
+  it("should set resource id from decorated dataset class", () => {
+    const res = new SodaResource(SampleDataset, null);
+    expect(res.Id.toString()).toEqual("abcd-efgh");
+  });
+
+  it("should throw if creating resource with undecorated dataset class", () => {
+    const createFunc = () =>
+      new SodaResource(BadDataset, null);
+
+      expect(createFunc).toThrow();
   });
 });
