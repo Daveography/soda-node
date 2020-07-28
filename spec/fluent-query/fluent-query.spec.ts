a61f9b4500a02d3edb5763de3091404d99dc0edb
Dave Sutherland <dave@daveography.ca>
2020-07-28 14:34:06 -0600

    Remove some redundancies in the tests

--- spec/fluent-query/fluent-query.spec.ts
+++ spec/fluent-query/fluent-query.spec.ts
@@ -20,18 +20,14 @@ describe("FluentQuery", () => {
       .limit(20)
       .offset(30);
 
-    const generatedQuery = (query as FluentQuery<ITestInterface>).toString();
-
-    expect(generatedQuery.toString()).toEqual("?$limit=20&$offset=30");
+    expect(query.toString()).toEqual("?$limit=20&$offset=30");
   });
 
   it("should generate simple query with one selected column", () => {
     const query = new FluentQuery<ITestInterface>(mockResource)
     .select(x => x.id);
 
-    const generatedQuery = (query as FluentQuery<ITestInterface>).toString();
-
-    expect(generatedQuery.toString()).toEqual("?$select=id");
+    expect(query.toString()).toEqual("?$select=id");
   });
 
   it("should generate simple query with chained selected columns", () => {
@@ -39,9 +35,7 @@ describe("FluentQuery", () => {
     .select(x => x.id)
     .select(x => x.title);
 
-    const generatedQuery = (query as FluentQuery<ITestInterface>).toString();
-
-    expect(generatedQuery.toString()).toEqual("?$select=id&$select=title");
+    expect(query.toString()).toEqual("?$select=id&$select=title");
   });
 
   it("should generate complex query with multiple clauses", () => {
@@ -52,9 +46,7 @@ describe("FluentQuery", () => {
       .select(x => x.id)
       .select(x => x.title);
 
-    const generatedQuery = (query as FluentQuery<ITestInterface>).toString();
-
-    expect(generatedQuery.toString())
+    expect(query.toString())
       .toEqual("?$limit=10&$offset=20&$select=id&$select=title&$where=id = '1'");
   });
 
@@ -74,5 +66,23 @@ describe("FluentQuery", () => {
 
     expect(query.toString())
       .toEqual("?$where=id = '1' OR title = 'some text'");
+  });
+
+  it("should combine mutliple location queries with or", () => {
+    const query = new FluentQuery<ITestInterface>(mockResource)
+      .location(x => x.location).withinCircle(new Location(53.540959, -113.493819), 2000)
+      .location(x => x.location).withinCircle(new Location(53.540859, -113.493719), 2000);
+
+    expect(query.toString())
+      .toEqual("?$where=within_circle(location, 53.540959, -113.493819, 2000) OR within_circle(location, 53.540859, -113.493719, 2000)");
+  });
+
+  it("should combine mutliple geometry queries with or", () => {
+    const query = new FluentQuery<ITestInterface>(mockResource)
+      .geometry(x => x.geometry).withinCircle(GeoJSONUtils.point(53.540959, -113.493819), 2000)
+      .geometry(x => x.geometry).withinCircle(GeoJSONUtils.point(53.540859, -113.493719), 2000);
+
+    expect(query.toString())
+      .toEqual("?$where=within_circle(location, 53.540959, -113.493819, 2000) OR within_circle(location, 53.540859, -113.493719, 2000)");
   });
 });
